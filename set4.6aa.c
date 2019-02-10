1c6d391b3c16c77874a88482d5282459ff2b71c8
rohininair98 <47473608+rohininair98@users.noreply.github.com>
2019-02-10 12:20:38 +0530

    Create set4.6aa.c

---
+++ set4.6aa.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char str[20];
+int calpha,cdigit,cspecial,cspace;
+int i;
+cdigit=calpha=cspecial=cspace=0;
+printf("enter a string");
+gets(str);
+for(i=0;str[i]!=NULL;i++)
+{
+if(str[i]>='0' && str[i]<='9')
+cdigit++;
+else if((str[i]>='A' && str[i]<='Z')||(str[i]>='a' && str[i]<='z'))
+calpha++;
+else if(str[i]==' ')
+cspace++;
+else
+cspecial++;
+}
+printf("%d",cspecial);
+return 0;
+}
