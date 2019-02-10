9c750c1ecdf5936395f1fbe318e2b33114de29bb
rohininair98 <47473608+rohininair98@users.noreply.github.com>
2019-02-10 17:13:10 +0530

    Create set10.2.c

---
+++ set10.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int n,sum=0,c,value;
+    scanf("%d",&n);
+    for(c=1;c<=n;c++)
+    {
+        scanf("%d",&value);
+        sum=sum+value;
+    }
+    printf("%d",sum);
+    return 0;
+}
