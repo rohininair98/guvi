353a88e4de7f87adf44d186baa7bb97de41a7dd4
rohininair98 <47473608+rohininair98@users.noreply.github.com>
2019-02-10 09:12:15 +0530

    Create set1.8.c

---
+++ set1.8.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main()
+{
+	int i,l=0,b;
+    scanf("%d",&b);
+    for(i=1;i<=b;i++)
+        {
+            l+=i;
+        }
+            printf("%d",l);
+    getch();
+}
