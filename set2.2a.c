683cbd77ec30e31bd3bbcd338c2fe51e2ff17eab
rohininair98 <47473608+rohininair98@users.noreply.github.com>
2019-02-10 09:36:28 +0530

    Create set2.2a.c

---
+++ set2.2a.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+	int b,l=0,a,i,tot=0,j;
+    scanf("%d",&a);
+    b=a;
+   while(b)
+    {
+     b=b/10;
+     l++;
+        }
+    b=a;
+    while(b)
+    {
+        i=b%10;
+        for(j=1;j<l;j++)
+        {
+        i*=10;
+        }
+        tot=tot+i;
+        l=l-1;
+        b/=10;
+        }
+        if(tot==a)
+        {
+            printf("yes");
+        }
+        else
+        {
+            printf("no");
+        }
+    return 0;
+}
