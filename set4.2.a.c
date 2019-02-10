19ea1bd18b0007c5082743f47696ba0f772815b8
rohininair98 <47473608+rohininair98@users.noreply.github.com>
2019-02-10 11:34:09 +0530

    Create set4.2.a.c

---
+++ set4.2.a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include<string.h>
+
+void main()
+{
+    char a[50];
+    int i=0,word=1;
+    printf("enter string");
+    gets(a);
+    while(str[i])!='\0')
+    {
+        if(str[i]==''||str[i]==1)
+        {
+            word++;
+        }
+            i++;
+        }
+        
+    }
+        printf("total words =%d");
+
+    getch();
+}
